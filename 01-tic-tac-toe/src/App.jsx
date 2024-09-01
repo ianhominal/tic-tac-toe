85e8b2b6d35dbad820281cb9f763625570664062
Ian Hominal <ianhominal@gmail.com>
2024-09-01 19:29:05 -0300

    project creation

---
+++ 01-tic-tac-toe/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import confetti from 'canvas-confetti';
+import {Square} from "./components/Square.jsx";
+import {TURNS} from "./constants.js";
+import {checkWinner, checkEndGame} from "./logic/board.js";
+import {WinnerModal} from "./components/WinnerModal.jsx";
+
+
+function App() {
+  
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
+
+  // null = no hay ganador, false = empate
+  const [winner, setWinner] = useState(null); 
+
+  
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  }
+
+  const updateBoard = (index) => {
+    //si ya tiene algo no actualizamos el tablero
+    if(board[index] || winner){return;}
+
+    //actualizamos el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    //verificamos si hay un ganador
+    const newWinner = checkWinner(newBoard);
+    if(newWinner)
+    {
+      setWinner(newWinner);
+      confetti();
+      return;
+    } else if(checkEndGame(newBoard))
+    {
+      setWinner(false);
+      return;
+    }
+
+    //cambiamos el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  }
+
+  return (
+    <main className='board'>
+      <h1> Ta Te Ti </h1>
+      <button onClick={resetGame}>Reiniciar</button>
+      <section className='game'>
+        {
+          board.map((_,index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+        </section>
+
+        <section className ="turn">
+          <Square isSelected = {turn === TURNS.X}>
+            {TURNS.X}
+            </Square>
+          <Square isSelected = {turn === TURNS.O}>
+            {TURNS.O}
+          </Square>
+        </section>
+
+        <WinnerModal resetGame={resetGame} winner={winner}/>
+    </main>
+  )
+}
+
+export default App
